7657b3b709b5015bcd21e17bbe650448741b6056
Olivier <rataj.olivier@gmail.com>
2022-09-04 22:10:42 +0200

    Document LayoutConsole.cs methods

--- LiteChat.Core/Console/LayoutConsole.cs
+++ LiteChat.Core/Console/LayoutConsole.cs
@@ -46,26 +46,53 @@ namespace LiteChat.Core.Console
 
         #region Methods
 
+        /// <summary>
+        /// Initializes the <see cref="LayoutConsole"/> with values specified for space, border and colors
+        /// </summary>
+        /// <param name="width">Width of this <see cref="LayoutConsole"/></param>
+        /// <param name="height">Height of this <see cref="LayoutConsole"/></param>
+        /// <param name="foreground">Foreground color to be used for this <see cref="LayoutConsole"/></param>
+        /// <param name="background">Background color to be used for this <see cref="LayoutConsole"/></param>
+        /// <param name="glyph">The border glyph that surrounds this <see cref="LayoutConsole"/></param>
+        /// <param name="title">The Title of this <see cref="LayoutConsole"/>, comparable to an h1 element</param>
         void Initialize(int width, int height, Color foreground, Color background, int glyph, string title)
         {
             gBorderGlyph = glyph;
             SetDefaultColors(foreground, background);
             GenerateBox(width, height);
             WindowTitle = title;
+
+            // Offset the cursor so it's within the box bounds
+            Cursor.Move(1, 1);
         }
 
+        /// <summary>
+        /// Sets all default colors for this <see cref="LayoutConsole"/>
+        /// </summary>
+        /// <param name="foreground">The foreground color (text color, border color) to be used</param>
+        /// <param name="background">The background color to be used</param>
         void SetDefaultColors(Color foreground, Color background)
         {
             DefaultForeground = foreground;
             DefaultBackground = background;
         }
 
+        /// <summary>
+        /// Draws the border surrounding this Layout Console
+        /// </summary>
+        /// <param name="width">width of the <see cref="LayoutConsole"/></param>
+        /// <param name="height">height of the <see cref="LayoutConsole"/></param>
         void GenerateBox(int width, int height)
         {
             rConsoleRectangle = new Rectangle(0, 0, width, height);
             this.DrawBox(rConsoleRectangle, ShapeParameters.CreateFilled(new ColoredGlyph(DefaultForeground, DefaultBackground, gBorderGlyph), new ColoredGlyph(DefaultForeground, DefaultBackground)));
         }
 
+        /// <summary>
+        /// Sets the Window Title, overwriting the previous one and replacing empty space with the border
+        /// </summary>
+        /// <param name="title">new title to be set</param>
+        /// <param name="upperCase">whether to uppercase the title</param>
         void SetWindowTitle(string title, bool upperCase = false)
         {
             if (String.IsNullOrEmpty(title)) return;
