52fd390b2f1b5e0d2cd97a57f7e93afacb6bbaf0
Olivier <rataj.olivier@gmail.com>
2022-09-04 21:37:39 +0200

    Initial commit

---
+++ LiteChat/Program.cs
@@ -0,0 +1,41 @@
+﻿using SadConsole;
+using SadRogue.Primitives;
+using System;
+using Console = SadConsole.Console;
+
+namespace LiteChat
+{
+    internal class Program
+    {
+       /*   Version definition:
+        *   b       = beta
+        *   #.00    = main release
+        *   0.##    = release
+        */
+
+        const string VERSION = "0.01b";
+
+        const int SCREEN_WIDTH = 120;
+        const int SCREEN_HEIGHT = 30;
+
+        private static void Main(string[] args)
+        {
+
+            Settings.WindowTitle = $"LiteChat - {VERSION}";
+            Settings.UseDefaultExtendedFont = true;
+
+            Game.Create(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+            Game.Instance.OnStart = Init;
+            Game.Instance.Run();
+            Game.Instance.Dispose();
+        }
+
+        private static void Init()
+        {
+            ScreenObject mainContainer = new ScreenObject();
+            Game.Instance.Screen = mainContainer;
+            Game.Instance.DestroyDefaultStartingConsole();
+        }
+    }
+}
