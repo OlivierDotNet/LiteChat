976b323b467399ec77c77664ec01134e79436f8c
Olivier <rataj.olivier@gmail.com>
2022-09-06 01:02:04 +0200

    LayoutConsole.Utils create Glyph

---
+++ LiteChat.Core/Utils/Glyph.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiteChat.Core.Utils
+{
+    public struct Glyph
+    {
+        int glyphValue;
+        public int Value { get { return glyphValue; } set { glyphValue = value; } }
+
+        public Glyph(int value)
+        {
+            glyphValue = value;
+        }
+
+        public static implicit operator int(Glyph g) => g.glyphValue;
+        public static implicit operator Glyph(int i) => new Glyph(i);
+    }
+}
